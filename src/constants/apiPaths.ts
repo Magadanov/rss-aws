05a03ff390a1b916baf23b10ebca6b925ee8f64d
Magadanov <magadanov03@mail.ru>
2025-03-22 18:37:42 +0500

    feat: add fetch

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://92v1so5v6f.execute-api.eu-west-2.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://pm2tkx9plk.execute-api.eu-west-2.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
