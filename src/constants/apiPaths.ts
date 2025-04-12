4e5bc5d9f62e46e5a6c8c35464e1587f02ec5972
Magadanov <magadanov03@mail.ru>
2025-04-12 15:51:39 +0500

    feat: add cart url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://92v1so5v6f.execute-api.eu-west-2.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://pm2tkx9plk.execute-api.eu-west-2.amazonaws.com",
+  import: "https://d4l735ngs1.execute-api.eu-west-2.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://magadanov-cart-api-develop.eu-west-2.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
